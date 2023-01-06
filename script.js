d30c5a452256d2b0c1db2bf87c1fd4db45071f18
Jack273 <36973540+Jack273@users.noreply.github.com>
2023-01-07 10:44:39 +1100

    Plays 5 rounds button logic works games plays 5 rounds and announces winner

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const scoreDiv = document.querySelector('.score')
+
 function getComputerChoice () {
     let choices = ["rock", "paper", "scissors"]
     let choice = Math.floor(Math.random() * choices.length)
@@ -12,36 +17,45 @@ function playGame (user) {
     }
     else if (user === "rock" && computer === "paper"){
         console.log("You lose, paper beats rock")
+        computerScore ++;
     }
     else if (user === "rock" && computer === "scissors"){
         console.log("You win, rock beats scissors")
+        playerScore ++;
     }
     else if (user === "paper" && computer === "paper"){
         console.log("its a tie, both paper")
     }
     else if (user === "paper" && computer === "scissors"){
         console.log("You lose, scissors beats paper")
+        computerScore ++;
     }
     else if (user === "paper" && computer === "rock"){
         console.log("You win, paper beats rock")
+        playerScore ++;
     }
     else if (user === "scissors" && computer === "scissors"){
         console.log("its a tie, both scissors")
     }
     else if (user === "scissors" && computer === "paper"){
         console.log("You win, scissors beats paper")
+        playerScore ++;
     }
     else if (user === "scissors" && computer === "rock"){
         console.log("You lose, rock beats scissors")
+        computerScore ++;
     }
-    
-
     else {
         console.log("tes")
     }
-    
+    scoreDiv.textContent = "The current score is " + playerScore + " for the player, and " + computerScore + " for the computer"
 
-
+    if (playerScore ===5){
+        scoreDiv.textContent = "You win"
+    }
+    else if (computerScore===5){
+        scoreDiv.textContent = "Game over"
+    }
 }
 const buttons = document.querySelectorAll('button')
 
