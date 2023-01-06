142afa135f056d4528aeba85602ad3f1bfe84fb6
Jack273 <36973540+Jack273@users.noreply.github.com>
2023-01-07 10:47:46 +1100

    cleaned up console logs

--- script.js
+++ script.js
@@ -13,41 +13,33 @@ function getComputerChoice () {
 function playGame (user) {
     computer = getComputerChoice();
     if (user === "rock" && computer === "rock"){
-        console.log("its a tie, both rock")
+        return;
     }
     else if (user === "rock" && computer === "paper"){
-        console.log("You lose, paper beats rock")
         computerScore ++;
     }
     else if (user === "rock" && computer === "scissors"){
-        console.log("You win, rock beats scissors")
         playerScore ++;
     }
     else if (user === "paper" && computer === "paper"){
-        console.log("its a tie, both paper")
+        return;
     }
     else if (user === "paper" && computer === "scissors"){
-        console.log("You lose, scissors beats paper")
         computerScore ++;
     }
     else if (user === "paper" && computer === "rock"){
-        console.log("You win, paper beats rock")
         playerScore ++;
     }
     else if (user === "scissors" && computer === "scissors"){
-        console.log("its a tie, both scissors")
+        return
     }
     else if (user === "scissors" && computer === "paper"){
-        console.log("You win, scissors beats paper")
         playerScore ++;
     }
     else if (user === "scissors" && computer === "rock"){
-        console.log("You lose, rock beats scissors")
         computerScore ++;
     }
-    else {
-        console.log("tes")
-    }
+    
     scoreDiv.textContent = "The current score is " + playerScore + " for the player, and " + computerScore + " for the computer"
 
     if (playerScore ===5){
